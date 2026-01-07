b16c5ef9050d450dd3ea3191967fd4f812c29e7a
Hector Ruiz <ruizcadenahector@gmail.com>
2026-01-07 15:40:03 -0500

    Initial commit - Spring Boot backend PieJuega

---
+++ src/main/java/com/example/PieJuega/PieJuegaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PieJuega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieJuegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieJuegaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PieJuega/PieJuegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PieJuega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieJuegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
